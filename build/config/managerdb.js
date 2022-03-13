ca92c8da643d5b1f2bef3c9f84b0a4102e5e3afa
Lopez <lopez@MacBook-Pro-de-Lopez.local>
2022-03-13 11:51:54 -0500

    ÂGeneracion PDF OK como base64

--- build/config/managerdb.js
+++ build/config/managerdb.js
@@ -17,15 +17,10 @@ const PDFDocument = require('pdfkit');
 const fecha = new Date();
 const fechaHoy = new Date();
 const diaFInal = fecha.getDate();
+const { Base64Encode } = require('base64-stream');
 class ManagerDB {
     static executeQuery(sql, parameters, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            // pool.one(sql, parameters).then(ing => res.json({
-            //     'nombre': ing.nombres,
-            //     'apeliidos': ing.apellidos,
-            //     'cedula': ing.identificacion,
-            //     'resol': ing.resolucion_nacional,
-            // }));
             connectiondb_1.default.one(sql, parameters).then(ing => {
                 const titulo = ing.titulo_id;
                 const universidad = ing.universidad_id;
@@ -50,11 +45,11 @@ class ManagerDB {
                                 var cuerpoCertif = 'Que ' + ing.apellidos + ' ' + ing.nombres + ' identificado(a) con la Cédula de Ciudadanía No ' + ing.identificacion + ' matriculado(a) como ' + titulo.titulo +
                                     ' bajo el número de matrícula ' + ing.numeromatricula + ' acredita a su titular para ejercer la profesión de ' + titulo.titulo + ' o como especialista en la misma área dentro del territorio nacional. Esta matricula fue expedida mediante la resolución No ' + ing.resolucion_nacional +
                                     ' del ' + diarResol + '/0' + mesResol + '/' + anioResol + ' expedida por el Consejo Profesional de Ingeniería de Transporte y Vías de Colombia, con base en el título conferido por ' + uni.nombre + '.';
-                                function generateCertif(dataCallback, endCallback) {
+                                function generateCertif() {
                                     // Inicialización del objeto doc
                                     const doc = new PDFDocument();
-                                    doc.on('data', dataCallback);
-                                    doc.on('end', endCallback);
+                                    // doc.on('data', dataCallback);
+                                    // doc.on('end', endCallback);
                                     // doc.fontSize(25).text('nombre' + ing.nombres, 90, 90);
                                     doc.image(imgPath, 0, 0, { width: doc.page.width, height: doc.page.height });
                                     doc.font('Helvetica-Bold').fontSize(10).text(encabezadoCetrif, 85, 120, {
@@ -74,13 +69,27 @@ class ManagerDB {
                                     doc.font('Helvetica-Bold').text('“Antes de imprimir este documento, asegúrese que sea necesario. Proteger el medio ambiente también es su responsabilidad”.', 90, 518);
                                     doc.text('Ing. MANUEL ARIAS MOLANO', 258, 680);
                                     doc.font('Helvetica').text('Director Ejecutivo');
+                                    // Pasar a base 64
+                                    var finalString = ''; // contains the base64 string
+                                    var stream = doc.pipe(new Base64Encode());
                                     doc.end();
+                                    stream.on('data', function (chunk) {
+                                        finalString += chunk;
+                                    });
+                                    stream.on('end', () => {
+                                        res.json(finalString);
+                                    });
                                 }
-                                const stream = res.writeHead(200, {
-                                    'Content-Type': 'application/pdf',
-                                    'Content-Disposition': 'attachment;filename=certif.pdf'
-                                });
-                                generateCertif((chunk) => stream.write(chunk), () => stream.end());
+                                // Finalizacion del objeto doc
+                                generateCertif();
+                                // const stream = res.writeHead(200, {
+                                //     // 'Content-Type': 'application/pdf',
+                                //     'Content-Type': 'multipart/form-data',
+                                //     'Content-Disposition': 'attachment;filename=certif.pdf'
+                                // })
+                                // generateCertif(
+                                //     (chunk: any) => stream.write(chunk), () => stream.end()
+                                // )
                             });
                         }
                         uniConsulta(query2, parametros2);
--- src/config/managerdb.ts
+++ src/config/managerdb.ts
@@ -4,17 +4,12 @@ const PDFDocument = require('pdfkit');
 const fecha: any = new Date();
 const fechaHoy: any = new Date();
 const diaFInal: any = fecha.getDate();
+const { Base64Encode } = require('base64-stream');
 
 
 class ManagerDB {
     protected static async executeQuery(sql: string, parameters: any, res: Response): Promise<any> {
 
-        // pool.one(sql, parameters).then(ing => res.json({
-        //     'nombre': ing.nombres,
-        //     'apeliidos': ing.apellidos,
-        //     'cedula': ing.identificacion,
-        //     'resol': ing.resolucion_nacional,
-        // }));
 
         pool.one(sql, parameters).then(ing => {
             const titulo: any = ing.titulo_id;
@@ -45,11 +40,12 @@ class ManagerDB {
                                 ' bajo el número de matrícula ' + ing.numeromatricula + ' acredita a su titular para ejercer la profesión de ' + titulo.titulo + ' o como especialista en la misma área dentro del territorio nacional. Esta matricula fue expedida mediante la resolución No ' + ing.resolucion_nacional + 
                                 ' del ' + diarResol + '/0' + mesResol + '/' + anioResol + ' expedida por el Consejo Profesional de Ingeniería de Transporte y Vías de Colombia, con base en el título conferido por '+ uni.nombre + '.'
                             
-                                function generateCertif(dataCallback: any, endCallback: any) {
+                                function generateCertif() {
+
                                 // Inicialización del objeto doc
                                 const doc = new PDFDocument();
-                                doc.on('data', dataCallback);
-                                doc.on('end', endCallback);
+                                // doc.on('data', dataCallback);
+                                // doc.on('end', endCallback);
                                 
                                 // doc.fontSize(25).text('nombre' + ing.nombres, 90, 90);
                                 doc.image(imgPath, 0, 0, { width: doc.page.width, height: doc.page.height });
@@ -71,32 +67,47 @@ class ManagerDB {
                                 doc.font('Helvetica').text('Este certificado se expide con base en la ley 842 de 2003, artículo 6, parágrafo único.', 100, 498)
                                 doc.font('Helvetica-Bold').text('“Antes de imprimir este documento, asegúrese que sea necesario. Proteger el medio ambiente también es su responsabilidad”.', 90, 518)
                                 doc.text('Ing. MANUEL ARIAS MOLANO', 258, 680);
-                                doc.font('Helvetica').text('Director Ejecutivo')
+                                doc.font('Helvetica').text('Director Ejecutivo')    
+                                // Pasar a base 64
+                                var finalString = ''; // contains the base64 string
+                                var stream = doc.pipe(new Base64Encode());
                                 doc.end();
+
+                                stream.on('data', function(chunk:any){
+                                    finalString += chunk
+                                })
+                                stream.on('end', ()=>{
+                                    res.json(finalString);
+                                })
                             }
+                            // Finalizacion del objeto doc
+                            generateCertif();
 
-                            const stream = res.writeHead(200, {
-                                'Content-Type': 'application/pdf',
-                                'Content-Disposition': 'attachment;filename=certif.pdf'
-                            })
 
-                            generateCertif(
-                                (chunk: any) => stream.write(chunk), () => stream.end()
-                            )
+
+                            // const stream = res.writeHead(200, {
+                            //     // 'Content-Type': 'application/pdf',
+                            //     'Content-Type': 'multipart/form-data',
+                            //     'Content-Disposition': 'attachment;filename=certif.pdf'
+                            // })
+
+                            // generateCertif(
+                            //     (chunk: any) => stream.write(chunk), () => stream.end()
+                            // )
 
                         })
                     }
-
                     uniConsulta(query2, parametros2);
                 })
 
             }
             tituloConsulta(query, parametros1);
+            
         });
 
-
+        
     }
-
+    
 
 }
 
